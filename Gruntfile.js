6851194bbc81d03013e916623b250176975d05aa
Pratik Borsasdiya <pratikborsadiya@gmail.com>
2018-08-15 03:01:06 +0530

    Update node dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+const sass = require('node-sass');
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -35,6 +36,7 @@ module.exports = function(grunt) {
 		sass: {
 			dist: {
 				options: {
+					implementation: sass,
 					outputStyle: 'expanded',
 					sourceMap: false
 				},
@@ -55,7 +57,7 @@ module.exports = function(grunt) {
 				]
 			},
 			dist: {
-				src: ['docs/css/*.css']
+				src: ['public/css/*.css']
 			}
 		}
 	});

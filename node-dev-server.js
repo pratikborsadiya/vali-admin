47426f45f988171e461c14c8cd9b0cc2ffe0f7f5
Pratik Borsadiya <pratikborsadiya@gmail.com>
2023-10-15 13:07:31 +0530

    Fix a few bugs

--- node-dev-server.js
+++ node-dev-server.js
@@ -1,12 +1,12 @@
 // Node JS development server for Vali Admin
 
-var http = require("http"),
-	url = require("url"),
-	path = require("path"),
-	fs = require("fs")
-	port = process.argv[2] || 8888;
+const http = require("http");
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
+const port = process.argv[2] || 8888;
 
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
 	var uri = url.parse(request.url).pathname,
 		filename = path.join(process.cwd(), 'docs', uri);
 
@@ -19,10 +19,10 @@ http.createServer(function(request, response) {
 		case '.ico': contentType = 'image/x-icon'; break;
 		case '.svg': contentType = 'image/svg+xml'; break;
 	}
-	
-	fs.exists(filename, function(exists) {
-		if(!exists) {
-			response.writeHead(404, {"Content-Type": "text/plain"});
+
+	fs.exists(filename, function (exists) {
+		if (!exists) {
+			response.writeHead(404, { "Content-Type": "text/plain" });
 			response.write("404 Not Found\n");
 			response.end();
 			return;
@@ -30,18 +30,18 @@ http.createServer(function(request, response) {
 
 		if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-		fs.readFile(filename, "binary", function(err, file) {
-			if(err) {
-				response.writeHead(500, {"Content-Type": "text/plain"});
+		fs.readFile(filename, "binary", function (err, file) {
+			if (err) {
+				response.writeHead(500, { "Content-Type": "text/plain" });
 				response.write(err + "\n");
 				response.end();
 				return;
 			}
-			response.writeHead(200, {'Content-Type': contentType});
+			response.writeHead(200, { 'Content-Type': contentType });
 			response.write(file, "binary");
 			response.end();
 		});
 	});
 }).listen(parseInt(port, 10));
 
-console.log("Server is running on http://localhost:" + port );
+console.log("Server is running on http://localhost:" + port);
